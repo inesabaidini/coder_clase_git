033e4c60a3d5dd8f6937c80e4034b540ff967a0b
Inés Sabaidini <sabaidinii@gmail.com>
2022-11-06 22:54:03 -0300

    agregue lo de las ramas

--- CLASE16-Git.py
+++ CLASE16-Git.py
@@ -34,3 +34,21 @@ res = 2 + 3
 #git commit
 
 print('Terminando problema')
+
+
+#git commit -m 'Mensaje que quiero dejar'. Este menssaje está bueno para yo decir qué cambios hice en esta nueva versión, agregué x clase, hice esto o aquello, etc.
+#Después de cada cambio voy a tener que hacer el git add y el git commit si o si, y ahi listo, voy a tener todo em el repositorio
+
+#Para ver los cambios, comando> git log
+#Esto me muestra el commit seguido de un hash (clave alfanumerica larga al lado del commit)
+#Abajo me muestra quien hizo el cambio, a que hora, y el texto q indica de q se trataba el cambio q hice.
+#wq oara salir, después de los :
+
+#Podria crear distintas ramas:
+#quiero probar cosas, ver de agregar cosas a ver q onda. se me agregan las ramas para desarrollo, y me ahorro de perder o romper mi codigo. Esta sería ponele mi rama principal. o
+#rama MASTER. Cómo creo las ramas: git branch me dice cual rama estoy
+#git branch -l me va a mostrar la rama en la q estoy yo y las otras ramas q hay
+#git checkout (nombre de la rama a la que me quiero ir a trabajar ahora)
+#Y pruebo hacer cambios, para ver que el documento que tenía en la main no se modifica pero se crea otro igual en la pruebas_random donde se van quedando los cambios
+
+#FLASHERÍSIMO HERMANO, si me vuelvo al checkout main se me pierde todo q loco
